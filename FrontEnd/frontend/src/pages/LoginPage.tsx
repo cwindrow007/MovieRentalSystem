9648df99b38e3cba188c6212bb1c8bef04b5a598
ethanby <ethanby2000@gmail.com>
2025-04-15 21:56:38 -0400

    Update LoginPage.tsx
    Just added form html for now

--- FrontEnd/frontend/src/pages/LoginPage.tsx
+++ FrontEnd/frontend/src/pages/LoginPage.tsx
@@ -1,7 +1,21 @@
 const LoginPage = () => {
     return(
         <div>
-            <h1>Login</h1>
+            <h1>Movie Rentals</h1>
+            <form>
+                <fieldset>
+                    <legend>Login</legend>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" required />
+                    <br/>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" required />
+                    <br/><button type="submit">Login</button><br/>
+                    <label>Forgot password?</label><br/>
+                    <button type="button">Create account</button>
+                </fieldset>
+
+            </form>
         </div>
     );
 };
