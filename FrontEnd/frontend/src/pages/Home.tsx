99481edc77723ea73a5abc9864bd75ba358c73dd
Chris <ChristoWindow@gmail.com>
2025-04-20 21:08:39 -0400

    Added TailWind

--- FrontEnd/frontend/src/pages/Home.tsx
+++ FrontEnd/frontend/src/pages/Home.tsx
@@ -1,11 +1,12 @@
 const Home = () => {
-    return(
+    return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
             <h1 className="text-4xl font-bold text-blue-600 mb-5">Tailwind Test Test!</h1>
-            <button className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rouded">
+            <button className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded">
                 Test Button
             </button>
         </div>
     );
 };
+
 export default Home;
