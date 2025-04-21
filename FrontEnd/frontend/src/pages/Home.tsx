bb8d15ca1e0f82206b4bb757ccb1d9eb625a7e17
Chris <ChristoWindow@gmail.com>
2025-04-20 20:47:27 -0400

    Added TailWind Support

--- FrontEnd/frontend/src/pages/Home.tsx
+++ FrontEnd/frontend/src/pages/Home.tsx
@@ -1,7 +1,10 @@
 const Home = () => {
     return(
-        <div>
-            <h1>Home</h1>
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+            <h1 className="text-4xl font-bold text-blue-600 mb-5">Tailwind Test Test!</h1>
+            <button className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rouded">
+                Test Button
+            </button>
         </div>
     );
 };
