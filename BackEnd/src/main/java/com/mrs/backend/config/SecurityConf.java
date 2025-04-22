76e0e74da364dee180eea97534c3624413b29a2a
Chris <ChristoWindow@gmail.com>
2025-04-22 16:19:45 -0400

    Need JWT

--- BackEnd/src/main/java/com/mrs/backend/config/SecurityConf.java
+++ BackEnd/src/main/java/com/mrs/backend/config/SecurityConf.java
@@ -1,7 +1,9 @@
-package com.mrs.backend;
+package com.mrs.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
@@ -17,5 +19,10 @@ public class SecurityConf {
                 );
 
         return http.build();
+        }
+
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
         }
     }
